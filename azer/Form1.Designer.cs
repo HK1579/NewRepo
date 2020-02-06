01171e32b7dd9e8a45027eac8b8c15625c362a2d
HK1579 <hamzakhodry@gmail.com>
2020-02-06 16:36:07 +0100

    MyProject

---
+++ azer/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace azer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DGV1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGV1
+            // 
+            this.DGV1.AllowUserToDeleteRows = false;
+            this.DGV1.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.DGV1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.DGV1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV1.Location = new System.Drawing.Point(12, 12);
+            this.DGV1.Name = "DGV1";
+            this.DGV1.ReadOnly = true;
+            this.DGV1.RowHeadersWidth = 62;
+            this.DGV1.RowTemplate.Height = 29;
+            this.DGV1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGV1.Size = new System.Drawing.Size(1353, 489);
+            this.DGV1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1377, 542);
+            this.Controls.Add(this.DGV1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGV1;
+    }
+}
+
---
+++ azer/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace azer
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"server=desktop-gvadpv4\isaac;database=northwind;integrated security=true;");
+        SqlDataAdapter dAdapter;
+        DataSet dSet = new DataSet();
+        DataTable dTable = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            dAdapter = new SqlDataAdapter("select oD.OrderID,oD.UnitPrice,oD.Quantity from [Order Details] as oD where UnitPrice between 9 and 10 and Quantity between 10 and 11", con);
+            //with DataSET
+            //dAdapter.Fill(dSet,"oDetails");
+            //DGV1.DataSource = dSet.Tables["oDetails"];
+
+            //with DataTable
+            dAdapter.Fill(dTable);
+            DGV1.DataSource = dTable;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataGridViewTextBoxColumn champ = new DataGridViewTextBoxColumn();
+            champ.HeaderText = "Montant";
+            DGV1.Columns.Add(champ);
+            //DGV1.Columns["Montant"].DisplayIndex = 1;
+            // DGV1.Rows[3].Cells[3].Value = "kharbacha";
+            //int quantity,rate;
+            //int price = quantity * rate;
+            //DGV1.Rows[e.RowIndex].Cells["price"].Value = price.ToString();
+            //==================================================================================================================
+            //          for (int i = 0; i < DGV1.Rows.Count-1;i++)
+            //{
+            //              int a = DGV1.Rows[i][0].va;
+            //              int b = DGV1.Rows[i][1].value;
+
+            //              int c = a * b;
+
+            //              c = DGV1.Rows[i][2].value;
+
+
+            //         }
+            //==========================================================================
+            //foreach (DataGridViewRow row in DGV1.Rows)
+            //{
+            //    //row.Cells[DGV1.Columns["Montant"].Index].Value = (Convert.ToDouble(row.Cells[DGV1.Columns["quantity"].Index].Value) * (Convert.ToDouble(row.Cells[DGV1.Columns["UnitPrice"].Index].Value)));
+
+            //}
+
+            double sum=0;
+            for (int i = 0; i < DGV1.Rows.Count-1 ; i++)
+            {
+                
+                //DGV1.Rows[i].Cells[5].Style.Font = new Font("Arial", 10, FontStyle.Bold);
+                DGV1.Rows[i].Cells[3].Value = double.Parse(DGV1.Rows[i].Cells[2].Value.ToString()) * double.Parse(DGV1.Rows[i].Cells[1].Value.ToString());
+                sum++;
+                //dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[5].Value = sum;
+
+                // DOESN'T WOOOORK ====>      DGV1.Rows[i].Cells["Montant"].Value = double.Parse(DGV1.Rows[i].Cells["quantity"].Value.ToString()) * double.Parse(DGV1.Rows[i].Cells["UnitPrice"].Value.ToString());
+                DGV1.Rows[DGV1.Rows.Count-1].Cells[2].Value = "Somme:";
+                DGV1.Rows[DGV1.Rows.Count-1].Cells[2].Style.Font = new Font("Arial", 20, FontStyle.Bold);
+               
+            }
+
+            for (int i = 0; i < DGV1.Rows.Count - 1; i++)
+            {
+
+                sum += double.Parse(DGV1.Rows[DGV1.Rows.Count - 1].Cells[3].Value.ToString());
+
+            }
+           
+            //DGV1.Rows[11].Cells[2].Value = "Somme:";
+            //DGV1.Rows[11].Cells[2].Style.Font = new Font("Arial", 20, FontStyle.Bold);
+
+        }
+    }
+}
+
